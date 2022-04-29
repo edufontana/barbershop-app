5313bf564c8e62d89f4d357f1db7e10b8cf23141
eduardo fontana <edufontana7@gmail.com>
2022-04-28 23:40:45 -0300

    style: home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
-import {Text} from 'react-native';
-import {Container} from './styles';
+import React, {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+
+import {
+  Container,
+  Scroller,
+  HeaderArea,
+  HeaderTitle,
+  SearchButton,
+  LocationArea,
+  LocationInput,
+  LocationFinder,
+} from './styles';
+
+import SearchIcon from '../../assets/search.svg';
+import MyLocationIcon from '../../assets/my_location.svg';
 
 export function Home() {
+  const navigation = useNavigation();
+
+  const [locationText, setLocationText] = useState('aaa');
+
   return (
     <Container>
-      <Text>aaa</Text>
+      <Scroller>
+        <HeaderArea>
+          <HeaderTitle>Encontre o seu barbeiro favorito</HeaderTitle>
+          <SearchButton
+            onPress={() => {
+              navigation.navigate('Search');
+            }}>
+            <SearchIcon width="26" height="26" fill="#fff" />
+          </SearchButton>
+        </HeaderArea>
+
+        <LocationArea>
+          <LocationInput
+            placeholder="onde você está?"
+            placeholderTextColor={'#fff'}
+            onChangeText={text => setLocationText(text)}
+            value={locationText}
+          />
+          <LocationFinder>
+            <MyLocationIcon width="24" height="24" fill="#fff" />
+          </LocationFinder>
+        </LocationArea>
+      </Scroller>
     </Container>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,4 +2,49 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+  background-color: #63c2d1;
+`;
+
+export const Scroller = styled.ScrollView`
+  padding: 20px;
+  flex: 1;
+`;
+
+export const HeaderArea = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderTitle = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: #fff;
+  width: 60%;
+`;
+
+export const SearchButton = styled.TouchableOpacity`
+  flex: 1;
+  align-items: flex-end;
+`;
+
+export const LocationArea = styled.View`
+  background-color: #4eadbe;
+  height: 60px;
+  border-radius: 30px;
+  flex-direction: row;
+  align-items: center;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-top: 30px;
+`;
+
+export const LocationInput = styled.TextInput`
+  flex: 1;
+  color: #fff;
+`;
+
+export const LocationFinder = styled.TouchableOpacity`
+  height: 24px;
+  width: 24px;
 `;
