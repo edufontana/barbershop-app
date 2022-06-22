ee89e00a9ae62e2f79b9aa0856d8fc3e00ed3726
edu <edufontana7@gmail.com>
2022-06-22 04:43:37 -0300

    style: modal services

--- src/components/BarberModal/index.tsx
+++ src/components/BarberModal/index.tsx
@@ -1,9 +1,47 @@
-import React from 'react';
-import {Modal} from './styles';
+import React, {useEffect} from 'react';
+import {
+  Modal,
+  ModalArea,
+  ModalBody,
+  CloseButton,
+  ModalItem,
+  UserInfo,
+  UserAvatar,
+  UserName,
+  ViewWrapper,
+} from './styles';
 import {useNavigation} from '@react-navigation/native';
+import ExpandIcon from '../../assets/expand.svg';
 
-export function BarberModal() {
+export function BarberModal({show, setShow, user, service}) {
   const navigation = useNavigation();
 
-  return <Modal />;
+  const handleCloseModal = () => {
+    setShow(!show);
+  };
+
+  return (
+    <Modal visible={show} transparent={true}>
+      <ModalArea>
+        <ModalBody>
+          <CloseButton onPress={handleCloseModal}>
+            <ExpandIcon width="40" height="40" fill="#000" />
+          </CloseButton>
+
+          <ModalItem>
+            <ViewWrapper>
+              <UserAvatar
+                source={{
+                  uri: user.avatar,
+                }}
+              />
+              <UserInfo>
+                <UserName>{user.name}</UserName>
+              </UserInfo>
+            </ViewWrapper>
+          </ModalItem>
+        </ModalBody>
+      </ModalArea>
+    </Modal>
+  );
 }
--- src/components/BarberModal/styles.ts
+++ src/components/BarberModal/styles.ts
@@ -1,3 +1,50 @@
 import styled from 'styled-components/native';
 
 export const Modal = styled.Modal``;
+
+export const ModalArea = styled.View`
+  background-color: rgba(0, 0, 0, 0.5);
+  flex: 1;
+  justify-content: flex-end;
+`;
+export const ModalBody = styled.View`
+  background-color: #83d6e3;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  min-height: 300px;
+  padding: 10px 20px 40px 20px;
+`;
+
+export const CloseButton = styled.TouchableOpacity`
+  height: 40px;
+  width: 40px;
+`;
+
+export const ModalItem = styled.View`
+  background-color: #fff;
+  border-radius: 10px;
+  margin-bottom: 15px;
+  padding: 10px;
+`;
+
+export const UserInfo = styled.Text`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 20px;
+  margin-right: 15px;
+`;
+
+export const UserName = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export const ViewWrapper = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
